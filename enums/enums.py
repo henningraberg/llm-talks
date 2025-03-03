9962f9e7789f8ae20f6ea29e97d0105ff1ecd6a9
Henning Råberg <henning.raberg@oneflow.com>
2025-03-03 08:54:36 -0500

    enums: add enums

--- enums/enums.py
+++ enums/enums.py
@@ -14,3 +14,33 @@ class ChatRole(Enum):
 
     def is_user(self) -> bool:
         return self == ChatRole.USER
+
+
+class TextAlignment(Enum):
+    RIGHT = 'right'
+    CENTER = 'center'
+    LEFT = 'left'
+
+    def is_right(self) -> bool:
+        return self == TextAlignment.RIGHT
+
+    def is_center(self) -> bool:
+        return self == TextAlignment.CENTER
+
+    def is_left(self) -> bool:
+        return self == TextAlignment.LEFT
+
+
+class TextColor(Enum):
+    AGENT_1_COLOR = 'green'
+    SYSTEM_COLOR = 'red'
+    AGENT_2_COLOR = 'cyan'
+
+    def is_agent_1_color(self) -> bool:
+        return self == TextColor.AGENT_1_COLOR
+
+    def is_system_color(self) -> bool:
+        return self == TextColor.SYSTEM_COLOR
+
+    def is_agent_2_color(self) -> bool:
+        return self == TextColor.AGENT_2_COLOR
